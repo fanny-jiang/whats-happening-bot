0ee95c70d9dc154ebaa22349cd9d56ffb9767e0f
Fanny Jiang <jiangf@bu.edu>
2017-04-27 18:27:32 -0400

    set up token

--- webhook.js
+++ webhook.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const webhookToken = require('./secrets').webhookToken
 
 const app = express()
 
@@ -14,97 +15,111 @@ const server = app.listen(process.env.port || 8080, () => {
 /*<--- webhook setup--->*/
 
 // get request to webhook
-app.get('/webhook', function (req, res) {
-  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'VERIFY TOKEN') {
-    console.log("Validating webhook");
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === webhookToken) {
+    console.log('Validating webhook');
     res.status(200).send(req.query['hub.challenge']);
   } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
+    console.error('Failed validation. Make sure the validation tokens match.');
     res.sendStatus(403);
   }
 });
 
-app.post('/webhook', function (req, res) {
-  var data = req.body;
-  // make sure this is a page subscription
-  if (data.object === 'page') {
-
-    // iterate over each entry -- there may be multiple if batched (???)
-    data.entry.forEach(function (entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
-
-      // iterate over each messaging event
-      if (event.message) {
-        receivedMessage(event);
-      } else {
-        console.log("Webhook received unknown event", event);
-      }
+app.post('/webhook', (req, res) => {
+  if (req.body.object === 'page') {
+    req.body.entry.forEach((entry) => {
+      entry.messaging.forEach((event) => {
+        if (event.message && event.message.text) {
+          sendMessage(event);
+        }
+      });
     });
     res.sendStatus(200);
   }
-});
+})
 
-// send message
-function receivedMessage(event) {
-  var senderId = event.sender.id;
-  var recipientId = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
+// app.post('/webhook', function (req, res) {
+//   var data = req.body;
+//   if (data.object === 'page') {
 
-  console.log('Received message for user %d and page %d at %d with message: ', senderId, recipientId, timeOfMessage);
-  console.log(JSON.stringify(message));
+//     // iterate over each entry -- there may be multiple if batched (???)
+//     data.entry.forEach(function (entry) {
+//       var pageID = entry.id;
+//       var timeOfEvent = entry.time;
 
-  var messageId = message.mid;
+//       // iterate over each messaging event
+//       if (event.message) {
+//         receivedMessage(event);
+//       } else {
+//         console.log("Webhook received unknown event", event);
+//       }
+//     });
+//     res.sendStatus(200);
+//   }
+// });
 
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
+// // send message
+// function receivedMessage(event) {
+//   var senderId = event.sender.id;
+//   var recipientId = event.recipient.id;
+//   var timeOfMessage = event.timestamp;
+//   var message = event.message;
 
-  if (messageText) {
-    switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-      default:
-        sendTextMessage(senderID, messageText);
-    }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, 'Message with attachment received');
-  }
-}
+//   console.log('Received message for user %d and page %d at %d with message: ', senderId, recipientId, timeOfMessage);
+//   console.log(JSON.stringify(message));
 
-function sendGenericMessage(recipient, message) {
-  // add more later
-}
+//   var messageId = message.mid;
 
-function sendTextMessage(recipientId, messageText) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText
-    }
-  };
-  callSendAPI(messageData);
-}
+//   var messageText = message.text;
+//   var messageAttachments = message.attachments;
 
-callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: "PAGE_ACCESS_TOKEN" },
-    method: 'POST',
-    json: messageData
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
+//   if (messageText) {
+//     switch (messageText) {
+//       case 'generic':
+//         sendGenericMessage(senderID);
+//         break;
+//       default:
+//         sendTextMessage(senderID, messageText);
+//     }
+//   } else if (messageAttachments) {
+//     sendTextMessage(senderID, 'Message with attachment received');
+//   }
+// }
 
-      console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
-    } else {
-      console.error("unable to send message.");
-      console.error(response);
-      console.error(error);
-    }
-  });
-}
+// function sendGenericMessage(recipient, message) {
+//   // add more later
+// }
+
+// function sendTextMessage(recipientId, messageText) {
+//   var messageData = {
+//     recipient: {
+//       id: recipientId
+//     },
+//     message: {
+//       text: messageText
+//     }
+//   };
+//   callSendAPI(messageData);
+// }
+
+// function callSendAPI(messageData) {
+//   request({
+//     uri: 'https://graph.facebook.com/v2.6/me/messages',
+//     qs: { access_token: "PAGE_ACCESS_TOKEN" },
+//     method: 'POST',
+//     json: messageData
+
+//   }, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       var recipientId = body.recipient_id;
+//       var messageId = body.message_id;
+
+//       console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
+//     } else {
+//       console.error("unable to send message.");
+//       console.error(response);
+//       console.error(error);
+//     }
+//   });
+// };
+
