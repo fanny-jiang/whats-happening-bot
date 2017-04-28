c9d24cf15d7fa88c2290787fb69084d0c25b56d2
Fanny Jiang <jiangf@bu.edu>
2017-04-28 17:23:48 -0400

    added sendMessage function. not echoing back sent messages

--- webhook.js
+++ webhook.js
@@ -1,14 +1,18 @@
+'use strict';
+
 const express = require('express')
 const bodyParser = require('body-parser')
-const webhookToken = require('./secrets').webhookToken
+const request = require('request')
+const VERIFY_TOKEN = require('./tokens').VERIFY_TOKEN
+const PAGE_ACCESS_TOKEN = require('./tokens').PAGE_ACCESS_TOKEN
 
-const app = express()
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = app.listen(process.env.port || 8080, () => {
-  console.log('server is listening on port %d in %s mode!', server.address().port, app.settings)
+const server = app.listen(process.env.PORT || 8080, () => {
+  console.log('server is listening on port %d in %s mode!', server.address().port, app.settings.env)
   // console.log('server is listening!')
 })
 
@@ -16,12 +20,12 @@ const server = app.listen(process.env.port || 8080, () => {
 
 // get request to webhook
 app.get('/webhook', (req, res) => {
-  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === webhookToken) {
+  if (req.query['hub.mode'] && req.query['hub.verify_token'] === VERIFY_TOKEN) {
     console.log('Validating webhook');
     res.status(200).send(req.query['hub.challenge']);
   } else {
     console.error('Failed validation. Make sure the validation tokens match.');
-    res.sendStatus(403);
+    res.status(403).end();
   }
 });
 
@@ -34,9 +38,32 @@ app.post('/webhook', (req, res) => {
         }
       });
     });
-    res.sendStatus(200);
+    res.status(200).end()
   }
 })
+
+
+function sendMessage(event) {
+  let sender = event.sender.id;
+  let text = event.message.text;
+
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: {
+      recipient: { id: sender },
+      message: { text: text }
+    }
+  }, function (error, response) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
 
 // app.post('/webhook', function (req, res) {
 //   var data = req.body;
