1f9760583183dc4b47e6d4e929d432b1ef5da7ec
Fanny Jiang <jiangf@bu.edu>
2017-04-28 17:42:30 -0400

    echo message back works

--- webhook.js
+++ webhook.js
@@ -13,7 +13,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const server = app.listen(process.env.PORT || 8080, () => {
   console.log('server is listening on port %d in %s mode!', server.address().port, app.settings.env)
-  // console.log('server is listening!')
 })
 
 /*<--- webhook setup--->*/
@@ -29,6 +28,8 @@ app.get('/webhook', (req, res) => {
   }
 });
 
+// post request to webhook
+
 app.post('/webhook', (req, res) => {
   if (req.body.object === 'page') {
     req.body.entry.forEach((entry) => {
@@ -41,7 +42,6 @@ app.post('/webhook', (req, res) => {
     res.status(200).end()
   }
 })
-
 
 function sendMessage(event) {
   let sender = event.sender.id;
@@ -63,90 +63,3 @@ function sendMessage(event) {
     }
   });
 }
-
-
-// app.post('/webhook', function (req, res) {
-//   var data = req.body;
-//   if (data.object === 'page') {
-
-//     // iterate over each entry -- there may be multiple if batched (???)
-//     data.entry.forEach(function (entry) {
-//       var pageID = entry.id;
-//       var timeOfEvent = entry.time;
-
-//       // iterate over each messaging event
-//       if (event.message) {
-//         receivedMessage(event);
-//       } else {
-//         console.log("Webhook received unknown event", event);
-//       }
-//     });
-//     res.sendStatus(200);
-//   }
-// });
-
-// // send message
-// function receivedMessage(event) {
-//   var senderId = event.sender.id;
-//   var recipientId = event.recipient.id;
-//   var timeOfMessage = event.timestamp;
-//   var message = event.message;
-
-//   console.log('Received message for user %d and page %d at %d with message: ', senderId, recipientId, timeOfMessage);
-//   console.log(JSON.stringify(message));
-
-//   var messageId = message.mid;
-
-//   var messageText = message.text;
-//   var messageAttachments = message.attachments;
-
-//   if (messageText) {
-//     switch (messageText) {
-//       case 'generic':
-//         sendGenericMessage(senderID);
-//         break;
-//       default:
-//         sendTextMessage(senderID, messageText);
-//     }
-//   } else if (messageAttachments) {
-//     sendTextMessage(senderID, 'Message with attachment received');
-//   }
-// }
-
-// function sendGenericMessage(recipient, message) {
-//   // add more later
-// }
-
-// function sendTextMessage(recipientId, messageText) {
-//   var messageData = {
-//     recipient: {
-//       id: recipientId
-//     },
-//     message: {
-//       text: messageText
-//     }
-//   };
-//   callSendAPI(messageData);
-// }
-
-// function callSendAPI(messageData) {
-//   request({
-//     uri: 'https://graph.facebook.com/v2.6/me/messages',
-//     qs: { access_token: "PAGE_ACCESS_TOKEN" },
-//     method: 'POST',
-//     json: messageData
-
-//   }, function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//       var recipientId = body.recipient_id;
-//       var messageId = body.message_id;
-
-//       console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
-//     } else {
-//       console.error("unable to send message.");
-//       console.error(response);
-//       console.error(error);
-//     }
-//   });
-// };
-
