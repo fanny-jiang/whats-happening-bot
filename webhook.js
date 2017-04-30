33d58463f27a653ab5527e2eecbd86ee655c792f
Fanny Jiang <jiangf@bu.edu>
2017-04-30 01:20:17 -0400

    added comments

--- webhook.js
+++ webhook.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -6,12 +6,13 @@ const request = require('request')
 const getRandomEvent = require('./utilities').getRandomEvent
 const convertDateTime = require('./utilities').convertDateTime
 
-// tokens
+// Tokens
 const tokens = require('./tokens')
-const VERIFY_TOKEN = tokens.VERIFY_TOKEN
-const PAGE_ACCESS_TOKEN = tokens.PAGE_ACCESS_TOKEN
-const AI_CLIENT_ACCESS_TOKEN = tokens.AI_CLIENT_ACCESS_TOKEN
-const EB_ANON_TOKEN = tokens.EB_ANON_TOKEN
+const VERIFY_TOKEN = tokens.VERIFY_TOKEN,
+  PAGE_ACCESS_TOKEN = tokens.PAGE_ACCESS_TOKEN,
+  AI_CLIENT_ACCESS_TOKEN = tokens.AI_CLIENT_ACCESS_TOKEN,
+  EB_ANON_TOKEN = tokens.EB_ANON_TOKEN,
+  AI_SESSION_ID = tokens.AI_SESSION_ID
 
 // APIs
 const apiai = require('apiai')
@@ -22,31 +23,31 @@ const nbrite = new Nbrite({ token: EB_ANON_TOKEN })
 
 
 // express app setup
-const app = express();
+const app = express()
 
 // bodyparser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // server
 const server = app.listen(process.env.PORT || 8080, () => {
   console.log('server is listening on port %d in %s mode!', server.address().port, app.settings.env)
 })
 
-/*<--- webhook setup--->*/
+/*<--- FB Webhook Setup--->*/
 
 // get request to webhook
 app.get('/webhook', (req, res) => {
   if (req.query['hub.mode'] && req.query['hub.verify_token'] === VERIFY_TOKEN) {
-    console.log('Validating webhook');
-    res.status(200).send(req.query['hub.challenge']);
+    console.log('Validating webhook')
+    res.status(200).send(req.query['hub.challenge'])
   } else {
-    console.error('Failed validation. Make sure the validation tokens match.');
-    res.status(403).end();
+    console.error('Failed validation. Make sure the validation tokens match.')
+    res.status(403).end()
   }
 });
 
-// post request to webhook
+// post request to webhook with response from API.ai
 
 app.post('/webhook', (req, res) => {
   if (req.body.object === 'page') {
@@ -55,28 +56,27 @@ app.post('/webhook', (req, res) => {
         if (event.message && event.message.text) {
           sendMessage(event);
         }
-      });
-    });
+      })
+    })
     res.status(200).end()
   }
 })
 
 function sendMessage(event) {
-  let sender = event.sender.id;
-  let text = event.message.text;
+  let sender = event.sender.id
+  let text = event.message.text
 
   let apiai = apiaiApp.textRequest(text, {
-    sessionId: 'greg_cat'
+    sessionId: AI_SESSION_ID
   });
 
   apiai.on('response', (res) => {
     // console.log('RESPONSE FROM EVENTBRITE: ', res.result)
 
-    // IF MAKING SMALL TALK
     let message = null;
 
+    // if response from AI matches activity intent and returns events data send message to User
     if (res.result.action === 'activity' && res.result.fulfillment.data) {
-      // if data was received from eventbrite
       let aiRes = res.result.fulfillment.data,
         aiEventName = aiRes.eventName ? aiRes.eventName : 'Event',
         aiEventDesc = aiRes.eventDesc,
@@ -102,11 +102,13 @@ function sendMessage(event) {
         }
       }
     } else {
+      // if no data received from Eventbrite, send API.ai response to user
       message = { text: res.result.fulfillment.speech, attachment: null };
     }
 
     // console.log('OUTPUT MESSAGE', message)
 
+    // send json object containing message to FB messenger post request
     request({
       url: 'https://graph.facebook.com/v2.6/me/messages',
       qs: { access_token: PAGE_ACCESS_TOKEN },
@@ -130,7 +132,7 @@ function sendMessage(event) {
 
 }
 
-/* echo sent message */
+/* Echo sent message */
 
 // function sendMessage(event) {
 //   let sender = event.sender.id;
@@ -154,8 +156,9 @@ function sendMessage(event) {
 // }
 
 
-/* <----- eventbrite api -----> */
+/* <----- Eventbrite api -----> */
 
+// POST request to API.ai with data from Eventbrite event results
 app.post('/ai', (req, res) => {
 
   if (req.body.result.action === 'activity') {
